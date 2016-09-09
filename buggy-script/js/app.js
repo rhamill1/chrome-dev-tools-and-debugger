0f842214a08c6cc5fabe8ee7c3318b72ea181cf8
rhamill1 <rhamill11@gmail.com>
2016-09-09 15:41:03 -0700

    debugged

--- buggy-script/js/app.js
+++ buggy-script/js/app.js
@@ -1,12 +1,16 @@
-widow.onload = function(){
+window.onload = function(){
   var main = document.getElementById('main');
   var roll = document.getElementById('roll');
   var add  = document.getElementById('add');
-  add.addEventListener("cluck", function(){
+  var check  = document.getElementById('checkForWin');
+  add.addEventListener("click", function(){
     addDie();
   });
   roll.addEventListener("click", function(){
     rollDie();
+  });
+  check.addEventListener("click", function(){
+  checkForWin();
   });
 
   function addDie(){
@@ -17,13 +21,24 @@ widow.onload = function(){
   };
 
   function rollDie() {
-    var dice = document.getElementByClassName('die');
+    var dice = document.getElementsByClassName('die');
     for (var i = 0; i < dice.length; i++) {
-      dice[i].innerHTML = Random();
-    };
+      dice[i].innerHTML = random();
+    }
   };
 
   function random(){
-    return Math.floor(Math.random() * (7));
+    return Math.floor(Math.random() * (7));}
+
+
+  function checkForWin() {
+    var comp = [];
+    var dice = document.getElementsByClassName('die');
+    for (var i = 0; i < dice.length; i++) {
+        comp.push(dice[i]);}
+    for (var i = 0; i < comp.length; i++)
+        if (comp[i] = )
   }
+
+
 };
